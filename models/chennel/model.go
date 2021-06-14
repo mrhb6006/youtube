1e14943e5acc190b73f2f14338c1e51f5c6524b3
mrhb2000 <mrhb6006@gmail.com>
2021-06-14 14:03:31 +0430

    channel repo init

---
+++ models/chennel/model.go
@@ -0,0 +1,10 @@
+package chennel
+
+type Channel struct {
+	ID           int64
+	Name         string
+	CreationDate string
+	Description  string
+	Avatar       string
+	CreatorID    int64
+}
---
+++ models/chennel/vitess.go
@@ -0,0 +1,27 @@
+package chennel
+
+import (
+	"database/sql"
+	"go.uber.org/zap"
+	"time"
+)
+
+func (pg *postgres) GetByID(ID int64) (Channel, bool, string, error) {
+	channel := Channel{}
+	err := pg.Conn.QueryRow("SELECT id,name,creationDate,description,avatar,creatorID FROM channel WHERE id=?", ID).Scan(
+		&channel.ID,
+		&channel.Name,
+		&channel.CreationDate,
+		&channel.Description,
+		&channel.Avatar,
+		&channel.CreatorID,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return channel, false, "", nil
+		}
+		zap.L().Error("get_channel_err", zap.Any("error:", err), zap.Any("time :", time.Now().UnixNano()))
+		return channel, false, "01", err
+	}
+	return channel, true, "", nil
+}
