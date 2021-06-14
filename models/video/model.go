0fa557983d3d6d65b3bd5adf08fbd61e90def674
mrhb2000 <mrhb6006@gmail.com>
2021-06-14 13:36:28 +0430

    init repo

--- models/video/model.go
+++ models/video/model.go
@@ -1,1 +1,11 @@
 package video
+
+type Video struct {
+	ID          int64
+	Title       string
+	Description string
+	UploadDate  string
+	Duration    int64
+	Thumbnail   string
+	StorageID   int64
+}
--- models/video/repository.go
+++ models/video/repository.go
@@ -1,1 +1,21 @@
 package video
+
+import (
+	"database/sql"
+	postgresDB "youtube/pkg/db"
+)
+
+type postgres struct {
+	Conn *sql.DB
+}
+
+var Repo Repository
+
+func init() {
+	Repo = &postgres{
+		Conn: postgresDB.GetPGConnection(),
+	}
+}
+
+type Repository interface {
+}
