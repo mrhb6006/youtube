4db2d3e708e0ecd81efe8de91c626abc1f4594c7
mrhb2000 <mrhb6006@gmail.com>
2021-06-14 17:30:48 +0430

    fix bug

--- models/channelVideo/vitess.go
+++ models/channelVideo/vitess.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (pg *postgres) Insert(channelVideo ChannelVideo) (string, error) {
-	_, err := pg.Conn.Exec("INSERT INTO video_channel (channel_id,video_id) VALUES ($1,$2)", channelVideo.ChannelID, channelVideo.VideoID)
+	_, err := pg.Conn.Exec("INSERT INTO channel_video (channel_id,video_id) VALUES ($1,$2)", channelVideo.ChannelID, channelVideo.VideoID)
 	if err != nil {
 		zap.L().Error("insert_videoChannel_err", zap.Any("error:", err), zap.Any("time :", time.Now().UnixNano()))
 		return "01", err
