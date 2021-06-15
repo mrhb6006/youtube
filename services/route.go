ee1bbd74a31dae7ac51a20b99d2a2c7cfa19b4f5
behzad <behzad13792000@gmail.com>
2021-06-15 13:01:08 +0430

    add /youtube/channel/deletevideo route

--- services/route.go
+++ services/route.go
@@ -15,4 +15,5 @@ func setUpRoute(app *fiber.App) {
 	channel := youtube.Group("/channel")
 	channel.Post("/create", Create)
 	channel.Post("/addvideo", AddVideoToChannel)
+	channel.Post("/deletevideo", DeleteVideoFromChannel)
 }
