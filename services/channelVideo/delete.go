f0d3b006300cb7d170b214f0a81b5aaf4697c4c7
behzad <behzad13792000@gmail.com>
2021-06-15 13:00:51 +0430

    create DeleteVideoFromChannel service

---
+++ services/channelVideo/delete.go
@@ -0,0 +1,40 @@
+package channelVideo
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"youtube/apiType/channelVideoType"
+	"youtube/apiType/commonType"
+	"youtube/models/channelVideo"
+	"youtube/pkg/response"
+)
+
+func DeleteVideoFromChannel(ctx *fiber.Ctx) error {
+	baseErrCode := "004"
+	request := channelVideoType.ChannelVideoRequest{}
+	channelVideoResponse := channelVideoType.ChannelVideoResponse{}
+	res := commonType.Response{}
+	errStr, code, err := request.DecodeValidate(ctx)
+	if err != nil {
+		return response.ErrorResponse(ctx, res, baseErrCode, "01", errStr, code)
+	}
+	exist, errStr, err := channelVideo.Repo.ExistenceCheck(channelVideo.ChannelVideo{
+		ChannelID: request.ChannelID,
+		VideoID:   request.VideoID,
+	})
+	if err != nil {
+		return response.ErrorResponse(ctx, res, baseErrCode, "02", errStr, 500)
+	}
+	if !exist {
+		return response.ErrorResponse(ctx, res, baseErrCode, "03", errStr, 200)
+	}
+	errStr, err = channelVideo.Repo.Delete(channelVideo.ChannelVideo{
+		ChannelID: request.ChannelID,
+		VideoID:   request.VideoID,
+	})
+	if err != nil {
+		return response.ErrorResponse(ctx, res, baseErrCode, "04", errStr, 500)
+	}
+	channelVideoResponse.OperationDone = true
+	res.Res = channelVideoResponse
+	return response.SuccessResponse(ctx, res)
+}
