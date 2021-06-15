670ac5cc45af66cb2f9d6d92c6c0859bc4de387a
behzad <behzad13792000@gmail.com>
2021-06-15 14:28:57 +0430

    create DecodeValidate func for UserChannelRequest type

---
+++ apiType/userChannelType/decodeValidate.go
@@ -0,0 +1,26 @@
+package userChannelType
+
+import (
+	"encoding/json"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"youtube/pkg/validate"
+)
+
+func (r *UserChannelRequest) DecodeValidate(ctx *fiber.Ctx) (errStr string, responseCode int, err error) {
+	err = json.Unmarshal(ctx.Body(), r)
+	if err != nil {
+		return "01", 400, err
+	}
+	err = validate.Struct(r)
+	if err != nil {
+		customError := err.(validator.ValidationErrors)
+		switch customError[0].StructField() + "," + customError[0].ActualTag() {
+		case "ChannelID,required":
+			return "02", 400, err
+		case "ChannelID,gt":
+			return "03", 400, err
+		}
+	}
+	return "", 200, nil
+}
