985e12475171ed775c4818e86b4a9d8565f77ff5
mrhb2000 <mrhb6006@gmail.com>
2021-06-16 18:36:20 +0430

    user Repo

---
+++ models/user/repository.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"database/sql"
+	postgresDB "youtube/pkg/db"
+)
+
+type postgres struct {
+	Conn *sql.DB
+}
+
+var Repo Repository
+
+func init() {
+	Repo = &postgres{
+		Conn: postgresDB.GetPGConnection(),
+	}
+}
+
+type Repository interface {
+	Insert(user User) (int64, string, error)
+	GetByUserName(userName string) (User, int64, string, error)
+}
