79bd175981f4dee7de4e044e98da84e2f43820aa
mrhb2000 <mrhb6006@gmail.com>
2021-06-16 13:19:41 +0430

    check register and login path in auth

--- pkg/auth/middleware.go
+++ pkg/auth/middleware.go
@@ -3,11 +3,16 @@ package auth
 import (
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
+	"strings"
 	"youtube/pkg/errorHandler"
 )
 
 func Middleware() fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
+		path := string(ctx.Request().URI().RequestURI())
+		if strings.Contains(path, "register") || strings.Contains(path, "login") {
+			return ctx.Next()
+		}
 		token := string(ctx.Request().Header.Peek("token"))
 		if token == "" {
 			zap.L().Debug("empty token")
