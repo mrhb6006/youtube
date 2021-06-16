98b9026986b1ea7beaa3fb93ada1c17e9f2a89c2
mrhb2000 <mrhb6006@gmail.com>
2021-06-16 19:13:17 +0430

    change user validation

--- apiType/userType/decodeValidate.go
+++ apiType/userType/decodeValidate.go
@@ -2,17 +2,23 @@ package userType
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"net/mail"
+	"strings"
 	"youtube/pkg/validate"
 )
 
-func (r *RegisterRequest) DecodeValidate(ctx *fiber.Ctx) (string, int, error) {
-	err := json.Unmarshal(ctx.Body(), r)
+const MinNameLength = 5
+const MinPassLength = 8
+
+func (user *RegisterRequest) DecodeValidate(ctx *fiber.Ctx) (string, int, error) {
+	err := json.Unmarshal(ctx.Body(), user)
 	if err != nil {
 		return "01", 400, err
 	}
-	err = validate.Struct(r)
+	err = validate.Struct(user)
 	if err != nil {
 		customError := err.(validator.ValidationErrors)
 		switch customError[0].StructField() + "," + customError[0].ActualTag() {
@@ -23,5 +29,23 @@ func (r *RegisterRequest) DecodeValidate(ctx *fiber.Ctx) (string, int, error) {
 		}
 	}
 
+	user.Username = strings.TrimSpace(user.Username)
+	user.Password = strings.TrimSpace(user.Password)
+	user.Email = strings.TrimSpace(user.Email)
+
+	if len(user.Username) < MinNameLength {
+		return "04", 200, errors.New("invalid user param")
+	} else if len(user.Password) < MinPassLength {
+		return "05", 200, errors.New("invalid user param")
+	}
+	if user.Email != "" {
+		if len(user.Email) < 5 {
+			return "06", 200, errors.New("invalid user param")
+		}
+		_, err := mail.ParseAddress(user.Email)
+		if err != nil {
+			return "07", 200, errors.New("invalid user param")
+		}
+	}
 	return "", 200, nil
 }
