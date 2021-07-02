84e4d77f25b9bd02c28e0469a66336e659a08a9f
behzad <behzad13792000@gmail.com>
2021-07-02 20:52:25 +0430

    add CommentLikeCountResponse APITYPE

--- apiType/userChannelType/userChannel.go
+++ apiType/userChannelType/userChannel.go
@@ -4,3 +4,7 @@ package userChannelType
 type UserChannelRequest struct {
 	ChannelID int64 `json:"channel_id" validate:"required,gt=0"`
 }
+
+type CommentLikeCountResponse struct {
+	MemberCount int64 `json:"member_count"`
+}
