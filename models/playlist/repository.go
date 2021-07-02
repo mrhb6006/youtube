e309b4efc4cc980efed43280341b5088b342c405
mrhb2000 <mrhb6006@gmail.com>
2021-07-02 17:15:34 +0430

    model added

---
+++ models/playlist/repository.go
@@ -0,0 +1,22 @@
+package playlist
+
+import (
+	"database/sql"
+	postgresDB "youtube/pkg/db"
+)
+
+type postgres struct {
+	Conn *sql.DB
+}
+
+var Repo Repository
+
+func init() {
+	Repo = &postgres{
+		Conn: postgresDB.GetPGConnection(),
+	}
+}
+
+type Repository interface {
+	Insert(playList PlayList) (int64, string, error)
+}
