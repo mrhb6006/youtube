91155cabdae82928b626a99394c8f23cb9e12229
behzad <behzad13792000@gmail.com>
2021-07-02 14:20:06 +0430

    add CountVideoSeensResponse APITYPE

--- apiType/seenVideoType/seenVideo.go
+++ apiType/seenVideoType/seenVideo.go
@@ -3,3 +3,6 @@ package seenVideoType
 type SeenVideoRequest struct {
 	VideoID int64 `json:"video_id" validate:"required,gt=0"`
 }
+type CountVideoSeensResponse struct {
+	Count int64 `json:"video_seens_count"`
+}
