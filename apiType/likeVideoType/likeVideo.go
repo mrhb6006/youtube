888cb04e6282cacbf51f24b25a2e76355154fcf8
behzad <behzad13792000@gmail.com>
2021-07-02 20:09:23 +0430

    add LikeVideoCount APITYPE

--- apiType/likeVideoType/likeVideo.go
+++ apiType/likeVideoType/likeVideo.go
@@ -4,3 +4,12 @@ type LikeVideoRequest struct {
 	Action  int64 `json:"action" validate:"oneof=0 1"`
 	VideoID int64 `json:"video_id" validate:"gt=0,required"`
 }
+
+type LikeVideoCountRequest struct {
+	Action  int64 `json:"action" validate:"oneof=0 1"`
+	VideoID int64 `json:"video_id" validate:"gt=0,required"`
+}
+
+type LikeVideoCountResponse struct {
+	LikeCount int64 `json:"like_count"`
+}
