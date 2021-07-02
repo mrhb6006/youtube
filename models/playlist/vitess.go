685e6ba0272b99654bf2d8fe6c4ecebb35ae8136
mrhb2000 <mrhb6006@gmail.com>
2021-07-02 20:05:14 +0430

    fix query

--- models/playlist/vitess.go
+++ models/playlist/vitess.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (pg *postgres) Insert(playList PlayList) (insertedID int64, errStr string, err error) {
-	err = pg.Conn.QueryRow("INSERT INTO playlist (name, is_default, is_public, creator_id) VALUES ($1,$2,$3,$4); RETURNING id", playList.Name, playList.IsDefault, playList.IsPublic, playList.CreatorID).Scan(&insertedID)
+	err = pg.Conn.QueryRow("INSERT INTO playlist (name, is_default, is_public, creator_id) VALUES ($1,$2,$3,$4) RETURNING id", playList.Name, playList.IsDefault, playList.IsPublic, playList.CreatorID).Scan(&insertedID)
 	if err != nil {
 		zap.L().Error("insert_playlist_err", zap.Any("error:", err), zap.Any("time :", time.Now().UnixNano()))
 		return 0, "01", err
